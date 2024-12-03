50b81d1f2312a59f24402b68864ca178cbb25baa
Haziq Hakimi <haziqhakimiyes@gmail.com>
2024-12-03 19:08:27 +0800

    add header

--- band_name_generator.py
+++ band_name_generator.py
@@ -1,3 +1,11 @@
+################################################################################
+# Project Name:  Band Name Generator
+# Description:   This project generates band name from simple user input
+# Author:        ziqkimi308
+# Created:       2024-12-03
+# Updated:       2024-12-03
+# Version:       1.0
+################################################################################
 print("Welcome to Band Name Generator!")
 city = input("Enter a city name: ")
 pet = input("Enter a pet name: ")
